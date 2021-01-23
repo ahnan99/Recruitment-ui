9b0f83bf7c2768be6496522882386db1bd3a703c
albertliu <xiaochuan_liu@hotmail.com>
2021-01-23 15:50:02 +0800

    Change default router

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,6 @@
 import MainView from "./MainView";
 import axios from 'axios'
+import { Route, Switch, Redirect } from 'react-router-dom'
 
 axios.defaults.baseURL = process.env.REACT_APP_ALIYUNHOST ? process.env.REACT_APP_ALIYUNHOST + ":8087" : "http://127.0.0.1:8087"
 axios.defaults.withCredentials = true
@@ -7,7 +8,10 @@ axios.defaults.withCredentials = true
 function App() {
   return (
     <div className="App">
-      <MainView />
+      <Switch>
+        <Redirect exact from="/" to="/homepage" />
+        <MainView />
+      </Switch>
     </div>
   );
 }
