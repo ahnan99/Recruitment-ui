560cd246728768ab7616303cbea129606f92f376
ahnan <ahnanliu@live.com>
2021-04-02 10:39:16 +0800

    REACT_APP_ALIYUNHOST_EHR

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,7 @@ import MainView from "./MainView";
 import axios from 'axios'
 import { Route, Switch, Redirect } from 'react-router-dom'
 
-axios.defaults.baseURL = process.env.REACT_APP_ALIYUNHOST ? process.env.REACT_APP_ALIYUNHOST + ":8087" : "http://127.0.0.1:8087"
+axios.defaults.baseURL = process.env.REACT_APP_ALIYUNHOST_EHR ? process.env.REACT_APP_ALIYUNHOST_EHR + ":8087" : "http://127.0.0.1:8087"
 axios.defaults.withCredentials = true
 
 function App() {
